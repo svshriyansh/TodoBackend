b65e473bb2ae6ca5ea40fdfe94c335397b7e6d8c
SHRIYANSH <shriyansh@Shriyansh-Macbook-15.local>
2025-06-25 00:09:49 +0530

    Updated end point

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
